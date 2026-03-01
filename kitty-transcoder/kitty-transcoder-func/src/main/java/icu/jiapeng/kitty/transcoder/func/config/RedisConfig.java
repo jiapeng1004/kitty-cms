c7fc98591f040ff8e278b2c07bccbcd86c275ad7
jiapeng <jiapeng@chinamcloud.com>
2026-03-01 13:19:52 +0800

    feat: 转码服务

--- kitty-transcoder/kitty-transcoder-func/src/main/java/icu/jiapeng/kitty/transcoder/func/config/RedisConfig.java
+++ kitty-transcoder/kitty-transcoder-func/src/main/java/icu/jiapeng/kitty/transcoder/func/config/RedisConfig.java
@@ -1,46 +1,13 @@
 package icu.jiapeng.kitty.transcoder.func.config;
 
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
 import org.redisson.codec.JsonJackson3Codec;
 import org.redisson.spring.starter.RedissonAutoConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
-    @Bean(name = {"redisTemplate"})
-    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-        redisTemplate.setKeySerializer(stringRedisSerializer);
-        redisTemplate.setHashKeySerializer(stringRedisSerializer);
-        FastJsonRedisSerializer<Object> fastJsonSerializer = new FastJsonRedisSerializer<>(Object.class);
-        fastJsonSerializer.getFastJsonConfig()
-                .getParserConfig()
-                .setAutoTypeSupport(true)
-        ;
-        fastJsonSerializer.getFastJsonConfig().setSerializerFeatures(
-                SerializerFeature.WriteClassName,
-                SerializerFeature.WriteEnumUsingToString,
-                SerializerFeature.WriteMapNullValue,
-                SerializerFeature.WriteDateUseDateFormat,
-                SerializerFeature.WriteNullListAsEmpty,
-                SerializerFeature.WriteNullStringAsEmpty,
-                SerializerFeature.WriteNullBooleanAsFalse,
-                SerializerFeature.WriteNullNumberAsZero,
-                SerializerFeature.DisableCircularReferenceDetect
-        );
-        redisTemplate.setValueSerializer(fastJsonSerializer);
-        redisTemplate.setHashValueSerializer(fastJsonSerializer);
-        redisTemplate.afterPropertiesSet();
-        return redisTemplate;
-    }
 
     @Bean
     public RedissonAutoConfigurationCustomizer myRedissonAutoConfigurationCustomizer() {
