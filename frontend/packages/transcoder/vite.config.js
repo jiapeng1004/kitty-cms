f989c4525da7d904b8a57aa993397a0c1a515621
jiapeng <jiapeng@chinamcloud.com>
2026-03-06 23:39:43 +0800

    fix: 转码前端vite配置异常

--- frontend/packages/transcoder/vite.config.js
+++ frontend/packages/transcoder/vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3002,
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:9703',
-        changeOrigin: true,
-        rewrite: (path) => '/kitty-transcoder' + path
-      }
+    plugins: [vue()],
+    server: {
+        port: 3002,
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:9703',
+                changeOrigin: true,
+                rewrite: (path) => path
+            }
+        }
     }
-  }
 })
